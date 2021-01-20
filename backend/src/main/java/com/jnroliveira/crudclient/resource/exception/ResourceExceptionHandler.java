ff747b0febd3642068aeab3ae4af312d354bffe9
Júnior de Oliveira <junior_siq90@hotmail.com>
2021-01-20 17:43:03 -0300

    Find Client by id, exception handling

---
+++ backend/src/main/java/com/jnroliveira/crudclient/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.jnroliveira.crudclient.resource.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jnroliveira.crudclient.service.exception.EntityNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Resource not found");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+
+}
