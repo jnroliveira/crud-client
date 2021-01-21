48e106075dde6c2394a3cc274df736c169d73757
Júnior de Oliveira <junior_siq90@hotmail.com>
2021-01-21 13:31:33 -0300

    Pagination

--- backend/src/main/java/com/jnroliveira/crudclient/service/ClientService.java
+++ backend/src/main/java/com/jnroliveira/crudclient/service/ClientService.java
@@ -1,14 +1,14 @@
 package com.jnroliveira.crudclient.service;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,15 +25,15 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Client not found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
 		return new ClientDTO(entity);
 	}
 
